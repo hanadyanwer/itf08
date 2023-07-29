e2a8d2758d1daa05480540723a1485229ad135d7
“[hanadyanwer]” <[hanadyanwer424@gmail.com]>
2023-07-29 11:53:16 +0300

    Test1 commit

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("test")
+
+name = input("Enter your Name")
+age = input("enter your age")
+birthday = input("Enter your birthday")
