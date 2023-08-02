df66c1ee2a0fc6aede8ef19ca4394ca4379909a2
“[hanadyanwer]” <[hanadyanwer424@gmail.com]>
2023-08-02 16:23:09 +0300

    Test2 commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+def add(num1:int,num2:int):
+    sum = num1+num2
+    return sum
+def subtraction(num1:int,num2:int):
+    subtraction = num1-num2
+    return subtraction
+def multiplication(num1: int,num2:int):
+    multiplication = num1*num2
+    return multiplication
+def division (num1:int,num2:int):
+    division = num1/num2
+    return division
+num1=input("enter number1")
+num2=input("enter number2")
+add(num1,num2)
+subtraction(num1,num2)
+multiplication(num1,num2)
+division(num1,num2)
+
+
